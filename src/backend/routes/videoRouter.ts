a294d170652805468091b2697f61ab258ea6b3e4
Daniel Herreros <daniel.herreros@owari.es>
2024-12-01 10:20:07 +0100

    fix: errores estructura video

--- src/backend/routes/videoRouter.ts
+++ src/backend/routes/videoRouter.ts
@@ -5,8 +5,12 @@ import { ApiResult } from '../types/ApiResult.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 import { validateNumericParams } from '../middlewares/validateNumericParams.js';
+import { verifyToken } from '../middlewares/verifyToken.js';
+
 
 const videoRouter = Express.Router();
+
+videoRouter.use(verifyToken);
 
 videoRouter.get("/", isLoggedIn, async (req: Express.Request, res: Express.Response) => {
     const result: ApiResult = await getAllVideos();
@@ -18,13 +22,8 @@ videoRouter.get("/:id", isLoggedIn, validateNumericParams, async (req: Express.R
     res.status(result.statusCode).json(result.data);
 });
 videoRouter.post("/", isLoggedIn, isAdmin, async (req: Express.Request, res: Express.Response) => {
-    // Ensure the video object includes all required properties, including userId
-    const video: Video = { 
-        userId: req.user.id, // Assuming req.user.id is available from isLoggedIn middleware
-        title: req.body.title, 
-        description: req.body.description, 
-        videoUrl: req.body.videoFile
-    };
+    const userId = (req as any).userId;
+    const video: Video = {userId: userId, title: req.body.title, description: req.body.description, videoUrl: req.body.videoFile};
     const result: ApiResult = await newVideo(video);
     res.status(result.statusCode).json({ message: result.message });
 });
@@ -35,12 +34,8 @@ videoRouter.delete("/:id", isLoggedIn, isAdmin, validateNumericParams, async (re
 });
 
 videoRouter.put("/:id", isLoggedIn, validateNumericParams, async (req: Express.Request, res: Express.Response) => {
-    const video: Video = { 
-        id: req.params.id, 
-        title: req.body.title, 
-        description: req.body.description, 
-        file_path: req.body.videoFile 
-    };
+    const userId = (req as any).userId;
+    const video: Video = { id: parseInt(req.params.id), userId: userId, title: req.body.title, description: req.body.description, videoUrl: req.body.videoFile };
     const result: ApiResult = await updateVideo(video);
     res.status(result.statusCode).json({ message: result.message });
 });
